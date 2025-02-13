5040fdead3e5438a34c8328a8d76bcce1a324a7b
strhzy <alexeydm2@gmail.com>
2025-02-13 14:48:20 +0300

    csproj upd

--- EldoMvideoAPI/obj/Debug/net8.0/EldoMvideoAPI.AssemblyInfo.cs
+++ EldoMvideoAPI/obj/Debug/net8.0/EldoMvideoAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EldoMvideoAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c72d48b865960494ebb8caf902d1c478540e5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd2b371a0d9b2c96924baf1318724697690f27cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EldoMvideoAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EldoMvideoAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
